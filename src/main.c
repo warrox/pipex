2e7db42a26e3dc5b1ef431d3271167de46af5b6b
Hamdi Warren <warren.hamdi@gmail.com>
2025-03-01 12:46:07 +0100

    [PIPEX]

--- src/main.c
+++ src/main.c
@@ -4,14 +4,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-int main(int argc, char **argv)
+int main(const int argc, char **argv)
 {
 	if(argc < 5){
 		printf("./Pipex <file1> <cmd1> <file2> <cmd2>\n");
 	}
-	int pid = open(argv[1],O_RDWR);
-	int pid_1 = open(argv[2], O_RDWR);
-	int rc = fork();
+	const int  pid = open(argv[1],O_RDWR);
+	const int pid_1 = open(argv[2], O_RDWR);
+	const int  rc = fork();
 	(void)pid;
 	(void)pid_1;
 	if(rc == -1){
@@ -20,8 +20,10 @@ int main(int argc, char **argv)
 	}	
 	else if(rc == 0){
 		printf("Child is call\n");
+		char *cmd1[] = {argv[3], 0}; // add correct bin to all exec function
+		execvp(cmd1[0], cmd1);
+		exit(EXIT_SUCCESS);
 	}
 	wait(NULL);
 	printf("Parent is called\n");
-
 }
