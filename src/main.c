a942bec5ddeae51943d2b20fa6f3fcbaddafcbf1
Hamdi Warren <warren.hamdi@gmail.com>
2025-03-02 09:30:07 +0100

    [PIPEX] -> test execvp

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 int main(const int argc, char **argv)
 {
+	char  *ptr[] = {"/bin/sh","-c","ls",NULL};
 	if(argc < 5){
 		printf("./Pipex <file1> <cmd1> <file2> <cmd2>\n");
 	}
@@ -19,10 +21,13 @@ int main(const int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}	
 	else if(rc == 0){
+		execvp("/bin/sh",ptr);
+		char *cmd1[] = {strcat("~/bin",argv[3]),0};
+		printf("CMD1 : %s", cmd1[0]);
+		write(1,"XXX\n",4);
 		printf("Child is call\n");
-		char *cmd1[] = {argv[3], 0}; // add correct bin to all exec function
-		execvp(cmd1[0], cmd1);
-		exit(EXIT_SUCCESS);
+		// execvp(cmd1[0], cmd1);
+		// exit(EXIT_SUCCESS);
 	}
 	wait(NULL);
 	printf("Parent is called\n");
